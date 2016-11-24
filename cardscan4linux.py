f9485609c2a1fb1ebae06e058019ad249ee2a94d
Kevin Sandom <kevin.sandom@made.com>
2016-11-24 16:09:41 +0000

    Fixed indentation of the lower section. (My original correction was for the wrong indentation width.)

--- cardscan4linux.py
+++ cardscan4linux.py
@@ -186,11 +186,11 @@ try:
                                                         print result
 
                 except KeyboardInterrupt:
-                    break
+                        break
                 except Exception as e:
-                    with open('cardscan4linux-error.log','a') as errlog:
-                        errlog.write("File: " + filepath + "\n" + e + "\n")
-                        sys.exit(bcolors.FAIL + "[*] " + bcolors.ENDC + "Cannot open file '" + filepath + "'.")
+                        with open('cardscan4linux-error.log','a') as errlog:
+                                        errlog.write("File: " + filepath + "\n" + e + "\n")
+                                        sys.exit(bcolors.FAIL + "[*] " + bcolors.ENDC + "Cannot open file '" + filepath + "'.")
 except:
         sys.exit(bcolors.WARNING + "\r[*] " + bcolors.ENDC + "There are no files that match the search.")
 
